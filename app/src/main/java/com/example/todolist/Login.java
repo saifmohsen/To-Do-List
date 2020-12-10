927665d8de35f5564878d7ac8a97ac4cae18d9b5
saifmohsen <saifmh1997@gmail.com>
2020-12-10 20:22:14 +0200

    make fourth screen and second commit

--- app/src/main/java/com/example/todolist/Login.java
+++ app/src/main/java/com/example/todolist/Login.java
@@ -3,12 +3,15 @@ package com.example.todolist;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Login extends AppCompatActivity {
     private TextView toSignUp;
+    private Button Login;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,12 +23,20 @@ public class Login extends AppCompatActivity {
 
         }
 
-
+        Login = (Button) findViewById(R.id.login);
         toSignUp = (TextView) findViewById(R.id.create_account);
         toSignUp.setOnClickListener(new View.OnClickListener() {
                                         @Override
                                         public void onClick(View view) {
                                             Intent intent = new Intent(getApplicationContext(), SignUp.class);
+                                            startActivity(intent);
+                                        }
+                                    }
+        );
+        Login.setOnClickListener(new View.OnClickListener() {
+                                        @Override
+                                        public void onClick(View view) {
+                                            Intent intent = new Intent(getApplicationContext(), Home.class);
                                             startActivity(intent);
                                         }
                                     }
